95b7b1d20b7547c185b5b0ff04ce7762e5ccc3aa
mac <mac@macdeMacBook-Air.local>
2018-05-27 16:23:22 +0800

    hello wrod

--- sericte/test001.py
+++ sericte/test001.py
@@ -1,7 +1,13 @@
-import  pytest
+import  pytest,allure
+
 class Test_001:
 
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a", [1, 2, 3])
     def test_001(self,a):
         assert a != 2
 
+    @allure.step(title="测试步骤002")
+    @pytest.mark.parametrize("a", [1, 6, 3])
+    def test_002(self, a):
+        assert a != 2
